20db86d6b06598fbd194712edfb1af21d20f4801
TheMarlboroMan <marlborometal@gmail.com>
2019-07-16 12:45:37 +0200

    Some empty files on topics I would like to discuss

---
+++ ___file_io.pl
@@ -0,0 +1,1 @@
+#This one will be about file input and output.
---
+++ ___prototypes.pl
@@ -0,0 +1,3 @@
+#This one will be about prototypes to subroutines.
+
+#We will also do a bit of passing blocks around as parameters to subroutines.
